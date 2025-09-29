d1b1a65f19ca9edd1b7f371c20203b97c2c4ab9d
TaimoorQadri123 <dchota374@gmail.com>
2025-09-29 17:46:35 +0500

    completed

--- index.js
+++ index.js
@@ -1,27 +1,40 @@
-var username = "Ali";
-var age = 2;
-var IsActive = true;
-var Address = "sharahefaisal";
-var Salary = null;
+// let username:string= "Ali";
+// let age:number= 2;
+// let IsActive:boolean=true;
+// let Address:any = "sharahefaisal";
+// let Salary:null= null;
 // console.log(username);
 // console.log(age);
 // console.log(IsActive);
 // console.log(Address);
 // console.log(Salary);
-var list = [1, 2, 3, 4, 5, 6];
+// let list:number[] =[1,2,3,4,5,6]
 // console.log(list[2]);
+// let tuplelist:[number,string,boolean] = [23,"Ali",true]
+// console.log(tuplelist[1]);
+// let obj:any = 
+// {
+//     "username": "Saad",
+//     "Age":12
+// }
+// console.log(obj["username"]);
+// function abc(){
+// console.log("HI world")
+// }
+// abc();
+// function Details(name:string,age:number)
+// {
+//     console.log(`Name: ${name}\nAge: ${age}`)
+// }
+// Details("Aina",23);
+for (var index = 1; index <= 10; index++) {
+    console.log(index);
+}
 var tuplelist = [23, "Ali", true];
-console.log(tuplelist[1]);
-var obj = {
-    "username": "Saad",
-    "Age": 12
-};
-console.log(obj["username"]);
-function abc() {
-    console.log("HI world");
+for (var i in tuplelist) {
+    console.log(tuplelist[i]);
 }
-abc();
-function Details(name, age) {
-    console.log("Name: ".concat(name, "\nAge: ").concat(age));
+for (var _i = 0, tuplelist_1 = tuplelist; _i < tuplelist_1.length; _i++) {
+    var i = tuplelist_1[_i];
+    console.log(i);
 }
-Details("Aina", 23);
--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
-let username:string= "Ali";
-let age:number= 2;
-let IsActive:boolean=true;
-let Address:any = "sharahefaisal";
-let Salary:null= null;
+// let username:string= "Ali";
+// let age:number= 2;
+// let IsActive:boolean=true;
+// let Address:any = "sharahefaisal";
+// let Salary:null= null;
 
 // console.log(username);
 // console.log(age);
@@ -11,30 +11,47 @@ let Salary:null= null;
 // console.log(Salary);
 
 
-let list:number[] =[1,2,3,4,5,6]
+// let list:number[] =[1,2,3,4,5,6]
 // console.log(list[2]);
+
+// let tuplelist:[number,string,boolean] = [23,"Ali",true]
+
+// console.log(tuplelist[1]);
+
+// let obj:any = 
+// {
+//     "username": "Saad",
+//     "Age":12
+// }
+// console.log(obj["username"]);
+
+
+// function abc(){
+// console.log("HI world")
+
+// }
+// abc();
+
+// function Details(name:string,age:number)
+// {
+//     console.log(`Name: ${name}\nAge: ${age}`)
+// }
+// Details("Aina",23);
+
+
+
+for (let index = 1; index<=10; index++){
+console.log(index)
+} 
+
 
 let tuplelist:[number,string,boolean] = [23,"Ali",true]
 
-console.log(tuplelist[1]);
-
-let obj:any = 
-{
-    "username": "Saad",
-    "Age":12
-}
-console.log(obj["username"]);
+for (const i in tuplelist){
+console.log(tuplelist[i])
+} 
 
 
-function abc(){
-console.log("HI world")
-
-}
-abc();
-
-function Details(name:string,age:number)
-{
-    console.log(`Name: ${name}\nAge: ${age}`)
-}
-Details("Aina",23);
-
+for (const i of tuplelist){
+console.log(i)
+} 
