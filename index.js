c739ca8e21c5dbf5229f0d427f77b5f855299518
TaimoorQadri123 <dchota374@gmail.com>
2025-09-29 17:11:30 +0500

    start intro with data type type script

---
+++ index.js
@@ -0,0 +1,14 @@
+var username = "Ali";
+var age = 2;
+var IsActive = true;
+var Address = "sharahefaisal";
+var Salary = null;
+// console.log(username);
+// console.log(age);
+// console.log(IsActive);
+// console.log(Address);
+// console.log(Salary);
+var list = [1, 2, 3, 4, 5, 6];
+// console.log(list[2]);
+var tuplelist = [23, "Ali", true];
+console.log(tuplelist[1]);
---
+++ index.ts
@@ -0,0 +1,21 @@
+let username:string= "Ali";
+let age:number= 2;
+let IsActive:boolean=true;
+let Address:any = "sharahefaisal";
+let Salary:null= null;
+
+// console.log(username);
+// console.log(age);
+// console.log(IsActive);
+// console.log(Address);
+// console.log(Salary);
+
+
+let list:number[] =[1,2,3,4,5,6]
+// console.log(list[2]);
+
+let tuplelist:[number,string,boolean] = [23,"Ali",true]
+
+console.log(tuplelist[1]);
+
+
